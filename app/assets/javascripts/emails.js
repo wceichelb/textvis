148cc36ed2c042e9f7000a5aaf959be3724be7fb
William Eichelberger <wceichelb@gmail.com>
2016-04-27 08:01:07 -0700

    user defined tree root: prefix/suffix/double

--- app/assets/javascripts/emails.js
+++ app/assets/javascripts/emails.js
@@ -1,10 +1,11 @@
-function drawChart(inpWord='') {
-  console.log(inpWord);
+function drawChart(inpWord='', root='suffix') {
+  console.log(inpWord, root);
   var data = google.visualization.arrayToDataTable(_.zip.apply(_, rawdata));
   var options = {
     wordtree: {
       format: 'implicit',
-      word: inpWord
+      word: inpWord,
+      type: root
     }
   };
 
@@ -12,11 +13,11 @@ function drawChart(inpWord='') {
   chart.draw(data, options);
 }
 
-function loadTree(word='') {
+function loadTree(word, root) {
   $("#vis_div").html("");
   $("#tree-root-control").show();
 
-  drawChart(word);
+  drawChart(word, root);
 }
 
 function loadCloud(word_array) {
